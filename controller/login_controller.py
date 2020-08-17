8d4cbf325a7fb25ae7eabf5965dd6b55287f6545
Joel Bispo <joelbisponeto@gmail.com>
2020-08-17 11:26:03 +0200

    deleting key firebase

--- controller/login_controller.py
+++ controller/login_controller.py
@@ -1,6 +1,13 @@
+
 class LoginController:
     def login(self, email: str, password: str):
         pass
 
     def logout(self, email:str):
+        pass
+
+    def get_user_logged(self):
+        pass
+
+    def is_this_user_logged(self):
         pass
